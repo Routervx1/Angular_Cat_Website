99c064071441a6fac24513c29f0adb23f8e74c11
Routervx1 <voum00@gmail.com>
2026-09-14 14:36:07 -0700

    Add files via upload

---
+++ src/app/nav/nav.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './nav.html',
+  styleUrl: './nav.css',
+})
+export class Nav {
+  protected readonly menuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.menuOpen.update((open) => !open);
+  }
+
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
+}
